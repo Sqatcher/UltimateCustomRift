6a6a0a95652849f7f310398848d277731b7701a9
Sqatcher <kacper.krzywicki@wp.pl>
2024-03-17 12:15:48 +0100

    Added Nocturne and title screen audio

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -7,6 +7,7 @@ public class GameDirector : MonoBehaviour
 {
     public static float difficultyLevel = 10f;
     public Text timerText;
+    public GameObject nocturne;
 
 
     // Start is called before the first frame update
@@ -16,6 +17,7 @@ public class GameDirector : MonoBehaviour
         GetComponent<M_Fiddlesticks>().Fiddlesticks();
         GetComponent<Timer>().resetTimer();
         GetComponent<Timer>().IsTimerOn(true);
+        nocturne.transform.GetComponent<M_Nocturne>().Nocturne();
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/M_Nocturne.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Nocturne : MonoBehaviour
+{
+    public GameObject areas;
+    public GameObject darkness;
+    public GameObject gameDirector;
+    public GameObject trueDarkness;
+    public GameObject nocturneLaughs;
+
+    int hauntingArea = -1;
+    bool isHaunting = false;
+    float hauntingTime = 0;
+    int nAreas;
+
+    float NOCTURNE_HAUNTING_NEEDED = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nAreas = areas.transform.childCount - 1;
+    }
+
+    void Update()
+    {
+        if (isHaunting)
+        {
+            hauntingTime += Time.deltaTime;
+            darkness.GetComponent<CanvasRenderer>().SetAlpha(hauntingTime / (NOCTURNE_HAUNTING_NEEDED/2));
+            Debug.Log(darkness.GetComponent<CanvasRenderer>().GetColor());
+        }
+
+        if (isHaunting && hauntingTime >= NOCTURNE_HAUNTING_NEEDED)
+        {
+            darkness.transform.SetParent(transform);
+            isHaunting = false;
+            Haunting();
+        }
+    }
+
+    public void Nocturne()
+    {
+        Invoke("StartHaunting", 4f);
+    }
+
+    void StartHaunting()
+    {
+        hauntingArea = Random.Range(0, nAreas);
+        darkness.transform.SetParent(areas.transform.GetChild(hauntingArea));
+        isHaunting = true;
+        hauntingTime = 0;
+        darkness.GetComponent<CanvasRenderer>().SetColor(new Color(1, 1, 1, 0));
+        darkness.SetActive(true);
+    }
+
+    void Haunting()
+    {
+        isHaunting = false;
+        darkness.SetActive(false);
+        trueDarkness.SetActive(true);
+        trueDarkness.transform.SetParent(areas.transform);
+        trueDarkness.transform.SetSiblingIndex(nAreas);
+        nocturneLaughs.transform.GetChild(Random.Range(0, nocturneLaughs.transform.childCount)).GetComponent<AudioSource>().Play();
+        Invoke("StopHaunting", 5f);
+        Invoke("StartHaunting", 8f);
+    }
+
+    void StopHaunting()
+    {
+        trueDarkness.SetActive(false);
+        trueDarkness.transform.SetParent(transform);
+    }
+}
